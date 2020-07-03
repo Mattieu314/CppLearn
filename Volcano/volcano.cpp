1e9b377a793decabcf7b77d4e536fb037abccc4f
Matthew Harris <matthewharris2002@hotmail.co.uk>
2020-07-03 09:32:30 +0100

    Added functions to get user input

--- Volcano/volcano.cpp
+++ Volcano/volcano.cpp
@@ -49,7 +49,7 @@ volcano::~volcano (){
     cout << "Destructing object of type volcano: " << name << endl;
 };
 
-/////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
  
 // volcanic range class
 class mountain_type{
@@ -66,6 +66,7 @@ class mountain_type{
         // Public member functions
         void add_volcano(volcano input_volcano);
         void update_number();
+        string get_name();
         int get_number();
         void get_info();
         double height_combined(vector<volcano> list_of_volcanoes);
@@ -111,6 +112,10 @@ int mountain_type::get_number(){
     return number_of_volcanoes;
 };
 
+string mountain_type::get_name(){
+    return name;
+};
+
 // Print info about each volcano within volcanic range
 void mountain_type::get_info(){
     cout << "=======================" <<endl;
@@ -139,22 +144,84 @@ double mountain_type::height_combined(vector<volcano> list_of_volcanoes){
     return total_height;
 };
 
+///////////////////////////////////////////////////////////////////////////////
+
+volcano get_volcano(){
+    string input_name;
+    double input_height;
+    cout << "Please enter the information for the volcano:" << endl;
+    cout << "Name of volano: ";
+    cin >> input_name;
+    cout << "\n Height of volcano (m): ";
+    cin >> input_height;
+    
+    volcano temp_volcano(input_name, input_height);
+
+    
+    return temp_volcano;
+};
+
+mountain_type get_mountain(){
+    string mountain_name;
+    cout << "What type of mountain would you like to enter? ";
+    cin >> mountain_name;
+
+    mountain_type temp_mountain(mountain_name);
+    return temp_mountain;
+};
+
+bool do_again(){
+    char user_again = 'n';
+    while (true){
+        cout << "Would you like to enter another volcano? (y/n) ";
+        cin >> user_again;
+        if ((user_again == 'y') || (user_again == 'Y') )
+            return true;
+        else if ((user_again == 'n') || (user_again == 'n'))
+            return false;
+        else{
+            cout << "This is an invalid option, please re-enter." << endl;
+            continue;
+        };
+    };
+};
+
 
 
 int main(){
-    volcano etna("Mt. Etna",3326);
-    volcano st_helens("Mount St. Helens", 2549);
+    bool again = false;
+    mountain_type temp_mountain = get_mountain();
+    
+    do{
+        volcano temp_volcano = get_volcano();
+        temp_mountain.add_volcano(temp_volcano);
+        cout << "You have entered the following volcano:" << endl;
+        temp_volcano.getinfo();
+        cout << "Which is of type: " << temp_mountain.get_name() << endl;
 
-    // construct a mountain_type object
-    mountain_type strato("Stratovolcano");
 
-    strato.add_volcano(etna);
-    cout << "Number of volcanoes: " << strato.get_number() << endl;
-
-    strato.add_volcano(st_helens);
-    cout << "Number of volcanoes: " << strato.get_number() << endl;
-
-    strato.get_info();
-
+        cout << "\n\n";
+        temp_mountain.get_info();
+        again = do_again();
+    } while (again == true);
     return 0;
 };
+
+
+/* int main(){ */
+/*     volcano etna("Mt. Etna",3326); */
+/*     volcano st_helens("Mount St. Helens", 2549); */
+
+/*     // construct a mountain_type object */
+/*     mountain_type strato("Stratovolcano"); */
+
+/*     strato.add_volcano(etna); */
+/*     cout << "Number of volcanoes: " << strato.get_number() << endl; */
+
+/*     strato.add_volcano(st_helens); */
+/*     cout << "Number of volcanoes: " << strato.get_number() << endl; */
+
+/*     strato.get_info(); */
+
+/*     return 0; */
+/* }; */
